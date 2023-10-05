89ace1e91a878a1ac77bc85f8088ca0703e2ff7f
john <johnhowardm@hotmail.com>
2023-10-05 16:44:48 +0100

    Changing debounce function to another version as first did not allow for argument in debounced function

--- app/utils/debounce.ts
+++ app/utils/debounce.ts
@@ -1,79 +1,19 @@
-type DebouncedCallback = {
-  flush: () => void | unknown;
-  cancel: () => void;
-  (): void | unknown;
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+export const debounce = (func, wait, immediate = null) => {
+  let timeout: number | null;
+  return function () {
+    let context = this,
+      args = arguments;
+    let later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout!);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 };
-type CallbackFunction = (arg: any) => unknown;
-type DebounceOptions = { maxWait?: number };
-
-/**
- * lodash.debounce causes a deployment error in Vercel. Below is a workaround.
- * Code below copied from:
- * https://github.com/getsentry/sentry-javascript/pull/6593/files/816309b64a4ed08a9868be5a51bc5098ee5c3490
- *
- * Heavily simplified debounce function based on lodash.debounce.
- *
- * This function takes a callback function (@param fun) and delays its invocation
- * by @param wait milliseconds. Optionally, a maxWait can be specified in @param options,
- * which ensures that the callback is invoked at least once after the specified max. wait time.
- *
- * @param func the function whose invocation is to be debounced
- * @param wait the minimum time until the function is invoked after it was called once
- * @param options the options object, which can contain the `maxWait` property
- *
- * @returns the debounced version of the function, which needs to be called at least once to start the
- *          debouncing process. Subsequent calls will reset the debouncing timer and, in case @paramfunc
- *          was already invoked in the meantime, return @param func's return value.
- *          The debounced function has two additional properties:
- *          - `flush`: Invokes the debounced function immediately and returns its return value
- *          - `cancel`: Cancels the debouncing process and resets the debouncing timer
- */
-export function debounce(
-  func: CallbackFunction,
-  wait: number,
-  options?: DebounceOptions
-): DebouncedCallback {
-  let callbackReturnValue: unknown;
-
-  let timerId: ReturnType<typeof setTimeout> | undefined;
-  let maxTimerId: ReturnType<typeof setTimeout> | undefined;
-
-  const maxWait =
-    options && options.maxWait ? Math.max(options.maxWait, wait) : 0;
-
-  function invokeFunc(): unknown {
-    cancelTimers();
-    callbackReturnValue = func(arg);
-    return callbackReturnValue;
-  }
-
-  function cancelTimers(): void {
-    timerId !== undefined && clearTimeout(timerId);
-    maxTimerId !== undefined && clearTimeout(maxTimerId);
-    timerId = maxTimerId = undefined;
-  }
-
-  function flush(): unknown {
-    if (timerId !== undefined || maxTimerId !== undefined) {
-      return invokeFunc();
-    }
-    return callbackReturnValue;
-  }
-
-  function debounced(): unknown {
-    if (timerId) {
-      clearTimeout(timerId);
-    }
-    timerId = setTimeout(invokeFunc, wait);
-
-    if (maxWait && maxTimerId === undefined) {
-      maxTimerId = setTimeout(invokeFunc, maxWait);
-    }
-
-    return callbackReturnValue;
-  }
-
-  debounced.cancel = cancelTimers;
-  debounced.flush = flush;
-  return debounced;
-}
