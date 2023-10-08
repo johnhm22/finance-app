4b0c39232a9a942aad51b3af981a1754ab58f079
john <johnhowardm@hotmail.com>
2023-10-08 10:24:53 +0100

    Removing redundant code

--- app/utils/jwt.helper.ts
+++ app/utils/jwt.helper.ts
@@ -1,5 +1,5 @@
 import { jwtVerify } from 'jose';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export const verifyToken = async (
   token: string
