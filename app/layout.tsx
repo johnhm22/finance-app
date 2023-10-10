457222e76342913f7eeaf7f01d5094febb703ea2
john <johnhowardm@hotmail.com>
2023-10-10 16:34:41 +0100

    Removing commented out code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,17 @@
-import { useContext, useState } from 'react';
-
 import Navbar from './components/Navbar';
 import './globals.css';
 import { UserIdProvider } from './components/UserContext';
-
-// export const metadata = {
-//   title: 'Stock Exchange',
-//   description: 'Check how rich you are',
-// };
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const [isUserLoggedIn, setUserLoggedIn] = useState(false);
-
-  // const userAuthentication = () => {
-  //   setUserLoggedIn(!isUserLoggedIn);
-  // };
-
   return (
     <html lang='en'>
       <body className='relative'>
         <UserIdProvider>
-          <Navbar
-          // isUserLoggedIn={isUserLoggedIn}
-          // userAuthentication={userAuthentication}
-          />
+          <Navbar />
           {children}
         </UserIdProvider>
       </body>
