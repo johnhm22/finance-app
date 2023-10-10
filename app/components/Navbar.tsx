af635713cee141b5795838d55436f28d2857e5c3
john <johnhowardm@hotmail.com>
2023-10-10 09:39:35 +0100

    Removing href link from logo as temp measure as currently if the user navigates here there is no means to return back to share list

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,11 +12,13 @@ const Navbar = () => {
   return (
     <header className='w-full z-10 bg-slate-100 fixed'>
       <nav className='max-w-[1440px] mx-auto flex justify-between items-center px-5  sm:px-16 h-14'>
-        <Link href='/' className='flex justify-center items-center'>
+        {/* <Link href='/' className='flex justify-center items-center'> */}
+        <div className='flex justify-center items-center'>
           <p className='font-extrabold text-orange-500 text-3xl'>a</p>
           <p className='font-extrabold text-orange-500 text-5xl mr-2'>i</p>
           <p className='font-bold'>activeInvestor</p>
-        </Link>
+        </div>
+        {/* </Link> */}
 
         <div className='self-end flex flex-row justify-end gap-3 mb-4 font-semibold text-base'>
           {!payloadData?.id ? (
@@ -27,14 +29,6 @@ const Navbar = () => {
               >
                 Log in
               </Link>
-              {/* <button className='border-solid border-2 px-4 py-1 rounded-full border-blue-700'>
-                <Link
-                  href='/login'
-                  className='flex justify-center items-center text-sm'
-                >
-                  Log in
-                </Link>
-              </button> */}
               <button className='text-white font-semibold px-4 py-1 rounded-full bg-blue-700'>
                 <Link
                   href='/register'
