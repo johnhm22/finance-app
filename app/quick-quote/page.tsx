70b45bf02e88e8de79cc63988a87b00e286c12df
john <johnhowardm@hotmail.com>
2023-10-17 09:35:33 +0100

    Adding new page for the quick quote feature

---
+++ app/quick-quote/page.tsx
@@ -0,0 +1,11 @@
+import QuickQuote from '@/app/components/QuickQuote';
+
+const Page = () => {
+  return (
+    <div className='flex max-container border-8 border-red-700 h-screen bg-slate-100 items-center justify-center'>
+      <QuickQuote />
+    </div>
+  );
+};
+
+export default Page;
