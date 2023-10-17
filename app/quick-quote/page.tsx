87137e19c0ffda53bbbc1ca77cca74d056d75a20
john <johnhowardm@hotmail.com>
2023-10-17 16:36:31 +0100

    Removing red border used to aid tailwind config

--- app/quick-quote/page.tsx
+++ app/quick-quote/page.tsx
@@ -2,7 +2,7 @@ import QuickQuote from '@/app/components/QuickQuote';
 
 const Page = () => {
   return (
-    <div className='flex max-container border-8 border-red-700 h-screen bg-slate-100 items-center justify-center'>
+    <div className='flex max-container h-screen bg-slate-100 items-center justify-center'>
       <QuickQuote />
     </div>
   );
