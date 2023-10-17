db4d9184325f18113e674245c5e1ce78ced10eb1
john <johnhowardm@hotmail.com>
2023-10-17 09:25:37 +0100

    Adding api to get price for a single share

---
+++ app/api/quick-quote/[id]/route.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  try {
+    const response = await axios({
+      url: `http://api.marketstack.com/v1/eod/latest?access_key=${process.env.MARKETSTACK_ACCESS_KEY}&symbols=${id}`,
+      method: 'GET',
+    });
+    return NextResponse.json({ response: response.data });
+  } catch (e) {
+    console.log('Error: ', e);
+  }
+}
