5a2ba4502e383d168beecf95426e3949f1e2b59f
john <johnhowardm@hotmail.com>
2023-10-18 14:15:27 +0100

    Adding validation to check that input values are present when the register form is submitted. If this is not the case the relevant error message is sent as a prop to the Input component and the error message displayed to the user

--- app/components/Register.tsx
+++ app/components/Register.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import React, { FormEvent, useRef, FormEventHandler } from 'react';
-import { IRegisterForm } from '@/types';
+import React, { useRef, useState } from 'react';
+import { IRegisterForm, RegisterErrors } from '@/types';
 import ButtonLarge from './ButtonLarge';
+import { validateRegister } from '../validations/validation-functions/auth.validations';
+import Input from './Input';
 
 interface IProps {
   onRegister: (data: IRegisterForm) => void;
@@ -11,12 +13,14 @@ interface IProps {
 const Register = ({ onRegister }: IProps) => {
   const registerComponentRef = useRef(null);
 
-  const [registerForm, setRegisterForm] = React.useState<IRegisterForm>({
+  const [registerForm, setRegisterForm] = useState<IRegisterForm>({
     username: '',
     password: '',
     firstName: '',
     lastName: '',
   });
+
+  const [errors, setErrors] = useState<RegisterErrors | null>();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
@@ -24,31 +28,31 @@ const Register = ({ onRegister }: IProps) => {
       ...prevState,
       [name]: value,
     }));
+    setErrors((prevState) => ({
+      ...prevState,
+      [name]: '',
+    }));
   };
-
-  // const handleClose = () => {
-  //   handleCloseEdit();
-  // };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onRegister(registerForm);
-    setRegisterForm({
-      username: '',
-      password: '',
-      firstName: '',
-      lastName: '',
-    });
+    const { errors, isValid } = validateRegister(registerForm);
+    if (isValid) {
+      onRegister(registerForm);
+      setRegisterForm({
+        username: '',
+        password: '',
+        firstName: '',
+        lastName: '',
+      });
+    } else {
+      setErrors({ ...errors });
+    }
   };
 
   return (
     <div className='flex flex-col bg-white w-1/3'>
-      <form
-        // data-modal-form={`modal-form${modalTitle && '-'}${modalTitle
-        //   .replace(/ +/g, '-')
-        //   .toLowerCase()}`}
-        onSubmit={handleSubmit}
-      >
+      <form onSubmit={handleSubmit}>
         <div className='p-5 pt-20'>
           <div className='flex justify-center items-center'>
             <p className='font-extrabold text-orange-500 text-3xl'>a</p>
@@ -57,56 +61,43 @@ const Register = ({ onRegister }: IProps) => {
           </div>
           <div className='self-center'>
             <div className='self-center'>
-              <label className='flex mt-10 text-sm capitalize mb-1 font-source'>
-                Username
-              </label>
-              <input
-                className='mt-1 px-3 py-2 bg-white border border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1'
-                id='username'
+              <Input
+                label='Username'
                 name='username'
-                type='text'
-                placeholder='username'
+                placeholder='Username'
                 onChange={handleChange}
+                type='text'
+                errors={errors?.username}
               />
             </div>
             <div className='self-center'>
-              <label className='flex mt-5 text-sm capitalize mb-1 font-source'>
-                First name
-              </label>
-              <input
-                className='mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1'
-                id='firstName'
+              <Input
+                label='First name'
                 name='firstName'
-                type='text'
-                placeholder='first name'
+                placeholder='First name'
                 onChange={handleChange}
+                type='text'
+                errors={errors?.firstName}
               />
             </div>
             <div className='self-center'>
-              <label className='flex mt-5 text-sm capitalize mb-1 font-source'>
-                Last name
-              </label>
-              <input
-                className='mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1'
-                id='lastName'
+              <Input
+                label='Last name'
                 name='lastName'
-                type='text'
-                placeholder='last name'
+                placeholder='Last name'
                 onChange={handleChange}
+                type='text'
+                errors={errors?.lastName}
               />
             </div>
-            <div className={`items-center self-start`}></div>
             <div className='self-center'>
-              <label className='flex mt-5 text-sm capitalize mb-1 font-source'>
-                Password
-              </label>
-              <input
-                className='mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1'
-                id='password'
+              <Input
+                label='Password'
                 name='password'
-                type='text'
-                placeholder='password'
+                placeholder='Password'
                 onChange={handleChange}
+                type='text'
+                errors={errors?.password}
               />
             </div>
           </div>
