d356efb87b350cf1404d46d722cf7e7595de6a6c
john <johnhowardm@hotmail.com>
2023-10-23 19:54:35 +0100

    Updating tailwind settings and making placeholder a prop

--- app/components/TickerSelect.tsx
+++ app/components/TickerSelect.tsx
@@ -23,6 +23,7 @@ const TickerSelect: React.FC<IProps> = ({
   handleTickerSearchText,
   tickerSearchText,
   tickerData,
+  placeholder,
 }) => {
   const handleSaveTicker = (ticker: TickerData) => {
     onTickerSelect(ticker);
@@ -32,8 +33,8 @@ const TickerSelect: React.FC<IProps> = ({
     <div>
       <div>
         <input
-          placeholder='Enter name or ticker symbol'
-          className='mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1'
+          placeholder={placeholder}
+          className='mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md text-sm md:text-lg focus:ring-1'
           onChange={handleTickerSearchText}
           value={
             tickerData?.symbol ? tickerData.symbol : tickerSearchText.ticker
