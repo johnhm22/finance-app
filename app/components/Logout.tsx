09e5e73ccbd450660faa157e36c41c49d3342862
john <johnhowardm@hotmail.com>
2023-10-23 13:58:17 +0100

    Deleting commented out code that is now replaced by the ActiveInvestor component

--- app/components/Logout.tsx
+++ app/components/Logout.tsx
@@ -27,13 +27,6 @@ const Logout = () => {
 
   return (
     <div className='flex flex-col bg-white md:w-9/12 lg:w-3/6 px-5 pt-20 sm:pt-28 w-full drop-shadow-md'>
-      {/* <div className='flex justify-center items-center'>
-        <p className='font-extrabold text-orange-500 text-lg  sm:text-3xl'>a</p>
-        <p className='font-extrabold text-orange-500 text-3xl md:text-5xl mr-2'>
-          i
-        </p>
-        <h2 className=' text-sm md:text-xl font-bold'>activeInvestor</h2>
-      </div> */}
       <ActiveInvestor />
       <div className='flex flex-col mt-10 bg-white justify-center items-center'>
         <div className='flex flex-1 text-sm md:text-xl font-semibold'>
