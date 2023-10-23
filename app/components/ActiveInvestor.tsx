338bb140e4e92421c03511d1f031b9551ae29383
john <johnhowardm@hotmail.com>
2023-10-23 13:51:58 +0100

    Creating component to display logo rather than repeating code for each display

---
+++ app/components/ActiveInvestor.tsx
@@ -0,0 +1,13 @@
+const ActiveInvestor = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <p className='font-extrabold text-orange-500 text-lg  sm:text-3xl'>a</p>
+      <p className='font-extrabold text-orange-500 text-3xl md:text-5xl mr-2'>
+        i
+      </p>
+      <h2 className=' text-sm md:text-xl font-bold'>activeInvestor</h2>
+    </div>
+  );
+};
+
+export default ActiveInvestor;
