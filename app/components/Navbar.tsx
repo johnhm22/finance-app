5f33bf942dc2e20956490e4654663b6c01816f21
john <johnhowardm@hotmail.com>
2023-10-23 11:21:32 +0100

    Updating Tailwind so that text is customised for smaller screens Also hiding username and welcome message in Navbar for smaller screens

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,43 +4,42 @@ import Link from 'next/link';
 
 import { useGlobalContext } from './UserContext';
 
-// import Image from 'next/image';
-// import CustomButton from "./CustomButton";
-
 const Navbar = () => {
   const { payloadData } = useGlobalContext();
   return (
     <header className='w-full z-10 bg-slate-100 fixed'>
-      <nav className='max-w-[1440px] mx-auto flex justify-between items-center px-5  sm:px-16 h-14'>
-        {/* <Link href='/' className='flex justify-center items-center'> */}
+      <nav className='max-w-[1440px] mx-2 md:mx-4 flex justify-between items-center md:px-5 sm:px-16 h-14'>
         <div className='flex justify-center items-center'>
-          <p className='font-extrabold text-orange-500 text-3xl'>a</p>
-          <p className='font-extrabold text-orange-500 text-5xl mr-2'>i</p>
-          <p className='font-bold'>activeInvestor</p>
+          <p className='font-extrabold text-orange-500 text-lg  sm:text-3xl'>
+            a
+          </p>
+          <p className='font-extrabold text-orange-500 text-3xl md:text-5xl mr-2'>
+            i
+          </p>
+          <p className='font-bold text-sm sm:text-lg'>activeInvestor</p>
         </div>
-        {/* </Link> */}
-
-        <div className='self-end flex flex-row justify-end gap-3 mb-4 font-semibold text-base'>
+        <div className='flex flex-row justify-end gap-3 font-semibold text-base'>
           {!payloadData?.id ? (
             <>
               <Link
                 href='/login'
-                className='flex justify-center items-center text-sm border-solid border-2 px-4 py-1 rounded-full border-blue-700'
+                className='flex justify-center items-center text-xs md:text-sm border-solid border-2 px-4 py-1 rounded-full border-blue-700'
               >
                 Log in
               </Link>
-              <button className='text-white font-semibold px-4 py-1 rounded-full bg-blue-700'>
-                <Link
-                  href='/register'
-                  className='flex justify-center items-center'
-                >
-                  Create account
-                </Link>
-              </button>
+
+              <Link
+                href='/register'
+                className='flex justify-center text-white font-semibold px-4 py-1 rounded-full bg-blue-700 items-center text-xs md:text-sm'
+              >
+                Create account
+              </Link>
             </>
           ) : (
             <>
-              <p className='mr-3'>Hi {payloadData.firstName}, welcome back</p>
+              <p className='mr-3 max-md:hidden'>
+                Hi {payloadData.firstName}, welcome back
+              </p>
               <Link
                 href='/logout'
                 className='flex justify-center items-center mr-3 text-red-500'
