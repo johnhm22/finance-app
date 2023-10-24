157e1cf71d671c69bca2cdc7c20c05adba8c4bae
john <johnhowardm@hotmail.com>
2023-10-24 16:09:22 +0100

    Adding Card component and making existing element a child of Card

--- app/components/Logout.tsx
+++ app/components/Logout.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import { deleteCookies } from '../utils/deleteCookies';
 import { useGlobalContext } from './UserContext';
 import ActiveInvestor from './ActiveInvestor';
+import Card from './Card';
 
 const Logout = () => {
   const { setPayloadData } = useGlobalContext();
@@ -26,20 +27,21 @@ const Logout = () => {
   }, []);
 
   return (
-    <div className='flex flex-col bg-white md:w-9/12 lg:w-3/6 px-5 pt-20 sm:pt-28 w-full drop-shadow-md'>
-      <ActiveInvestor />
-      <div className='flex flex-col mt-10 bg-white justify-center items-center'>
-        <div className='flex flex-1 text-sm md:text-xl font-semibold'>
-          Thanks for visiting, goodbye!
+    <Card>
+      {
+        <div className='flex flex-col mt-10 bg-white justify-center items-center'>
+          <div className='flex flex-1 text-sm md:text-xl font-semibold'>
+            Thanks for visiting, goodbye!
+          </div>
+          <Link
+            className='flex bg-blue-700 mt-10 sm:mt-20 justify-center items-center rounded-full w-full sm:w-10/12 p-3 md:p-5 hover:bg-blue-800 text-xs lg:text-lg text-white'
+            href='/'
+          >
+            Back to welcome page
+          </Link>
         </div>
-        <Link
-          className='flex bg-blue-700 mt-10 sm:mt-20 text-center items-center rounded-full h-12 w-full sm:w-10/12 p-3 md:p-5 hover:bg-blue-800 text-xs lg:text-lg text-white'
-          href='/'
-        >
-          Back to welcome page
-        </Link>
-      </div>
-    </div>
+      }
+    </Card>
   );
 };
 
