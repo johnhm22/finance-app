69dd2ec643066616a6ec9445330632c1e4c49a26
john <johnhowardm@hotmail.com>
2023-10-24 16:10:45 +0100

    Removing unnecessary div tag

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -6,10 +6,8 @@ import Logout from '../components/Logout';
 
 const Page = () => {
   return (
-    <div className='flex max-container h-screen bg-slate-100 items-center justify-center'>
-      <div className='flex justify-center w-3/5 h-3/4'>
-        <Logout />
-      </div>
+    <div className='flex flex-1 max-container h-screen bg-slate-100 items-center justify-center'>
+      <Logout />
     </div>
   );
 };
