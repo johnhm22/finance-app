8a871afe42624587526f424ff0e54541133dc37b
john <johnhowardm@hotmail.com>
2023-10-24 16:10:22 +0100

    Adding Card component and making existing element a child of Card

--- app/components/Register.tsx
+++ app/components/Register.tsx
@@ -6,6 +6,7 @@ import ButtonLarge from './ButtonLarge';
 import { validateRegister } from '../validations/validation-functions/auth.validations';
 import Input from './Input';
 import ActiveInvestor from './ActiveInvestor';
+import Card from './Card';
 
 interface IProps {
   onRegister: (data: IRegisterForm) => void;
@@ -52,56 +53,60 @@ const Register = ({ onRegister }: IProps) => {
   };
 
   return (
-    <div className='flex flex-col bg-white w-2/3  sm:w-3/5 md:w-1/3'>
-      <form onSubmit={handleSubmit}>
-        <div className='p-5 pt-20'>
-          <ActiveInvestor />
-          <div className='self-center'>
+    <Card>
+      {
+        <form
+          onSubmit={handleSubmit}
+          className='flex flex-col bg-white justify-center items-center w-full'
+        >
+          <div className='w-3/4 md:w-2/3 h:1/2 lg:h-full'>
             <div className='self-center'>
-              <Input
-                label='Username'
-                name='username'
-                placeholder='Username'
-                onChange={handleChange}
-                type='text'
-                errors={errors?.username}
-              />
+              <div className='self-center'>
+                <Input
+                  label='Username'
+                  name='username'
+                  placeholder='Username'
+                  onChange={handleChange}
+                  type='text'
+                  errors={errors?.username}
+                />
+              </div>
+              <div className='self-center'>
+                <Input
+                  label='First name'
+                  name='firstName'
+                  placeholder='First name'
+                  onChange={handleChange}
+                  type='text'
+                  errors={errors?.firstName}
+                />
+              </div>
+              <div className='self-center'>
+                <Input
+                  label='Last name'
+                  name='lastName'
+                  placeholder='Last name'
+                  onChange={handleChange}
+                  type='text'
+                  errors={errors?.lastName}
+                />
+              </div>
+              <div className='self-center mb-10'>
+                <Input
+                  label='Password'
+                  name='password'
+                  placeholder='Password'
+                  onChange={handleChange}
+                  type='text'
+                  errors={errors?.password}
+                />
+              </div>
             </div>
-            <div className='self-center'>
-              <Input
-                label='First name'
-                name='firstName'
-                placeholder='First name'
-                onChange={handleChange}
-                type='text'
-                errors={errors?.firstName}
-              />
-            </div>
-            <div className='self-center'>
-              <Input
-                label='Last name'
-                name='lastName'
-                placeholder='Last name'
-                onChange={handleChange}
-                type='text'
-                errors={errors?.lastName}
-              />
-            </div>
-            <div className='self-center mb-10'>
-              <Input
-                label='Password'
-                name='password'
-                placeholder='Password'
-                onChange={handleChange}
-                type='text'
-                errors={errors?.password}
-              />
-            </div>
+            <ButtonLarge label='Create Account' />
           </div>
-          <ButtonLarge label='Create Account' />
-        </div>
-      </form>
-    </div>
+        </form>
+      }
+    </Card>
   );
 };
 
