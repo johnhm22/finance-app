9c02d90cee310601dc64996241c8ac1649365a40
john <johnhowardm@hotmail.com>
2023-10-27 11:51:12 +0100

    Creating ticker search that is not restricted to London Stock Exchange XLON Currently, search in portfolio page is for XLON only as there is no way to distinguish shares and so currencies from different exchanges

---
+++ app/utils/ticker.search.quick-quote.helper.ts
@@ -0,0 +1,15 @@
+'use server';
+
+import axios from 'axios';
+
+export const findTickerQuickQuote = async (data: string) => {
+  try {
+    const response = await axios({
+      url: `http://api.marketstack.com/v1/tickers?search=${data}&access_key=${process.env.MARKETSTACK_ACCESS_KEY}&limit=5`,
+      method: 'GET',
+    });
+    return response.data;
+  } catch (error) {
+    console.log('ERROR: ', error);
+  }
+};
