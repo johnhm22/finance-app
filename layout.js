83ef50a909e84f582d05f3e1bba936e315eac7d8
john <johnhowardm@hotmail.com>
2023-10-28 19:25:00 +0100

    Adding file for Open Graph protocol data

---
+++ layout.js
@@ -0,0 +1,18 @@
+export const metadata = {
+  openGraph: {
+    title: 'Your personal finance app',
+    description: 'This is where you can manage your portfolio',
+    url: 'https://finance-app-puce.vercel.app/',
+    siteName: 'my finance app',
+    images: [
+      {
+        url: 'https://finance-app-puce.vercel.app/',
+        width: 800,
+        height: 600,
+        alt: 'My landing page',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+};
