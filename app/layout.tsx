0d3a4877fb82c0aa55a920168517955f96dcb8b4
john <johnhowardm@hotmail.com>
2023-10-30 14:02:08 +0000

    Adding metadata config for open graph protocol info which will improve display when website is sent in a link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,29 @@
+import { Metadata } from 'next';
+
 import Navbar from './components/Navbar';
 import './globals.css';
 import { UserIdProvider } from './components/UserContext';
+
+export const metadata: Metadata = {
+  title: 'My finance app',
+  description:
+    'Designed primarily as a learning exercise to improve knowledge of Next.js v13 and tailwind.css. The functionality enables a user to look up share prices and create a portfolio.',
+  authors: [{ name: 'John Morgan' }],
+  openGraph: {
+    type: 'website',
+    url: 'https://example.com',
+    title: 'My finance app',
+    description:
+      'Designed primarily as a learning exercise to improve knowledge of Next.js v13 and tailwind.css. The functionality enables a user to look up share prices and create a portfolio.',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/dbu5rpxkg/image/upload/v1698662802/finance_ngrwxn.png',
+        width: '1200',
+        height: '630',
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
