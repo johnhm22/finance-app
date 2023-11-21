38b0fedcc66ea1d98fc0fcc5e87ca65465acb3e9
john <johnhowardm@hotmail.com>
2023-11-21 15:32:35 +0000

    Addings https prefix to url to replace http

--- app/utils/ticker.search.quick-quote.helper.ts
+++ app/utils/ticker.search.quick-quote.helper.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 export const findTickerQuickQuote = async (data: string) => {
   try {
     const response = await axios({
-      url: `http://api.marketstack.com/v1/tickers?search=${data}&access_key=${process.env.MARKETSTACK_ACCESS_KEY}&limit=5`,
+      url: `https://api.marketstack.com/v1/tickers?search=${data}&access_key=${process.env.MARKETSTACK_ACCESS_KEY}&limit=5`,
       method: 'GET',
     });
     return response.data;
