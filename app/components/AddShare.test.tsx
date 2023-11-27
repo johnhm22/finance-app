08c4a0a6df413a6a24d966dff8eb99a141ecb240
john <johnhowardm@hotmail.com>
2023-11-27 15:59:54 +0000

    Adding further tests

--- app/components/AddShare.test.tsx
+++ app/components/AddShare.test.tsx
@@ -1,25 +1,130 @@
-import { render, screen } from '@testing-library/react';
-import user from '@testing-library/user-event';
+import React from 'react';
+import { render, screen, waitFor, within } from '@testing-library/react';
+import { expect, vi, test, describe } from 'vitest';
+import { userEvent } from '@testing-library/user-event';
 
 import AddShare from './AddShare';
-import test from 'node:test';
-import { expect } from 'vitest';
-import AuthProvider, { AuthContext } from './AuthProvider';
 
-// interface IProps {
-//     handleCloseAddShare: () => void;
-//     onSubmit: (arg: AddShareForm, userId: string) => void;
-//     setAddShareForm: React.Dispatch<React.SetStateAction<AddShareForm>>;
-//     addShareForm: AddShareForm | undefined;
-//     tickerSearch: (arg: string) => Promise<TickerResponse | undefined>;
-//   }
+describe('AddShare tests', () => {
+  const handleCloseAddShare = vi.fn();
+  const onSubmit = vi.fn();
+  const setAddShareForm = vi.fn();
 
-test('it shows two inputs and a button', () => {
-  render(<AddShare />);
+  const ticker = {
+    country: '',
+    has_eod: false,
+    has_intraday: false,
+    name: '',
+    symbol: '',
+    stock_exchange: {
+      acronym: '',
+      city: '',
+      country: '',
+      country_code: '',
+      mic: '',
+      name: '',
+      website: '',
+    },
+  };
 
-  const inputs = screen.getAllByRole('textbox');
-  const button = screen.getByRole('button');
+  const addShareForm = {
+    bookCost: 0,
+    quantity: 0,
+    ticker: ticker,
+  };
 
-  expect(inputs).toHaveLength(2);
-  //   expect(button).toBeDefined();
+  const tickerSearch = vi.fn();
+
+  vi.mock('./UserContext', () => {
+    return {
+      useGlobalContext: () => {
+        return {
+          payloadData: {
+            exp: 0,
+            firstName: '',
+            iat: 0,
+            id: '',
+            lastName: '',
+            role: '',
+            username: '',
+          },
+        };
+      },
+    };
+  });
+
+  const renderComponent = () => {
+    return render(
+      <AddShare
+        handleCloseAddShare={handleCloseAddShare}
+        onSubmit={onSubmit}
+        setAddShareForm={setAddShareForm}
+        addShareForm={addShareForm}
+      />
+    );
+  };
+
+  test('it renders without crashing', () => {
+    renderComponent();
+  });
+
+  test('Component heading is visible', () => {
+    renderComponent();
+    const heading = screen.getByRole('heading', {
+      name: 'Add new shareholding',
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('it displays input for new share', async () => {
+    const user = userEvent.setup();
+    renderComponent();
+    const shareInput = screen.getByPlaceholderText(
+      /Enter name or share symbol/i
+    );
+    user.click(shareInput);
+
+    await user.type(shareInput, 'Barclays');
+    expect(shareInput).toHaveValue('Barclays');
+  });
+
+  test('it calls api and displays list of companies when search text entered and on select of company, symbol is stored in input field', async () => {
+    const user = userEvent.setup();
+    renderComponent();
+    const shareInput = screen.getByPlaceholderText(
+      /enter name or share symbol/i
+    );
+
+    user.click(shareInput);
+
+    await user.type(shareInput, 'Barclays');
+    expect(shareInput).toHaveValue('Barclays');
+
+    const list = await screen.findByRole('list');
+
+    const { getAllByRole } = within(list);
+    const items = getAllByRole('listitem');
+    expect(items.length).toBe(1);
+    user.click(items[0]);
+    await waitFor(() => {
+      expect(shareInput).toHaveValue('BARC.XLON');
+    });
+    screen.debug();
+  });
+
+  test.only('it accepts entries for cost and quantity', async () => {
+    const user = userEvent.setup();
+    renderComponent();
+
+    const cost = screen.getByPlaceholderText(/book cost/i);
+    const quantity = screen.getByPlaceholderText(/quantity/i);
+
+    user.click(cost);
+    await user.type(cost, '555');
+    expect(cost).toHaveValue('555');
+
+    user.click(quantity);
+    await user.type(quantity, '1000');
+    expect(quantity).toHaveValue('1000');
+  });
 });
