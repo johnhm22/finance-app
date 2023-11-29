4312c8be89e041cf0071710cdef53943a3ceab00
john <johnhowardm@hotmail.com>
2023-11-29 14:56:44 +0000

    Adding mock api route for api/portfolio

--- app/mocks/handlers.ts
+++ app/mocks/handlers.ts
@@ -13,6 +13,33 @@ export const handlers = [
     return HttpResponse.json({ response: apiResponse });
   }),
 
+  http.get(`/api/portfolio/`, ({ request, params, cookies }) => {
+    const stocksHeld = [
+      {
+        id: '12345',
+        symbol: 'LLOY.XLON',
+        share_name: 'LLOYDS BANKING GROUP PLC ORD 10P',
+        bookCost: 20,
+        quantity: 60,
+        userId: '678910',
+        price: 45,
+      },
+      {
+        id: '54321',
+        symbol: 'RYA.XLON',
+        share_name: 'RYANAIR HOLDINGS PLC ORD EUR0.006',
+        bookCost: 90,
+        quantity: 10,
+        userId: '678910',
+        price: 125.5,
+      },
+    ];
+
+    const data = { stocksHeld };
+
+    return HttpResponse.json({ stocksHeld });
+  }),
+
   http.get(
     `https://api.marketstack.com/v1/tickers`,
     ({ request, params, cookies }) => {
