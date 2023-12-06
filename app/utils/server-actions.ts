524843842dd6a63014fe0ccd0235ef2e251165a9
john <johnhowardm@hotmail.com>
2023-12-06 16:47:22 +0000

    Adding server action to manage sending of contact form email using Resend package

---
+++ app/utils/server-actions.ts
@@ -0,0 +1,27 @@
+'use server';
+
+import { Resend } from 'resend';
+import ContactFormEmail from '../emails/contact-form-email';
+
+export const sendMessage = async (data: FormData) => {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  try {
+    const senderEmail = data.get('email');
+    const subject = data.get('subject');
+    const message = data.get('message');
+
+    const emailSent = await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: [senderEmail as string],
+      subject: subject as string,
+      react: ContactFormEmail({
+        email: senderEmail as string,
+        subject: subject as string,
+        message: message as string,
+      }),
+    });
+    return { success: true };
+  } catch (error) {
+    return { success: false, error };
+  }
+};
