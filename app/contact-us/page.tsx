015b5eb6ede3e4325a98441d44f19bc9d1e81549
john <johnhowardm@hotmail.com>
2023-12-07 14:07:57 +0000

    Creating a ContactUs component

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactUs from '../components/ContactUs';
+
+const Page = () => {
+  return (
+    <div className='flex flex-1 max-container h-screen items-center justify-center bg-slate-100'>
+      <ContactUs />
+    </div>
+  );
+};
+
+export default Page;
